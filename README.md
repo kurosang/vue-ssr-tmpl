c8a6a887611e40d22cc7fe90dfb578a496ac0232
kurosang <369750231@qq.com>
2020-12-22 18:21:35 +0800

    faet: 改造vue.config & env

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # 将 ssr-study 工程化，编写 ssr 模板
 
-学习这篇[VUE-CLI3 SSR 改造之旅](http://www.ediaos.com/2019/01/27/vue-cli3-ssr-project/)，继续改造。
+学习这篇[VUE-CLI3 SSR 改造之旅](http://www.ediaos.com/2019/01/27/vue-cli3-ssr-project/)，将 vue-ssr-study 的学习项目改造成可以运用到实战的模板。
 
 `npm run build`之后复制 dist 目录，然后安装依赖，运行`node server/index.js`就可以启动服务器
-
-初步根据上面两篇文章搭建完之后，距离实际使用还有很大的差距。
 
 ### ENV Config 构建
 
@@ -16,7 +14,40 @@
 修改 package.json 中的脚本，根据项目实际环境设置 NODE_DEPLOY，比如：cross-env `NODE_DEPLOY=test npm run dev`
 增加 config 文件夹，并增加 index.js 以及 env 代码&目录参考如下，基于 NODE_DEPLOY 获取到环境变量，config/index.js 根据环境变量获取 env 配置文件，默认合并 env.js，这与 cli3 提供 env 基本一致，但不受限于命名。
 
+改造之后的 scripts 如下：
+
+```
+{
+    "dev:client": "vue-cli-service serve --mode dev",
+    "dev:ssr": "cross-env BUILD_TARGET=node NODE_ENV=dev node ./server/ssr.js",
+    "dev:all": "concurrently \"vue-cli-service serve --mode dev --port 8081\" \"npm run dev:ssr\" ",
+    "dev": "cross-env NODE_DEPLOY=dev npm run dev:all",
+
+    "build:client": "vue-cli-service build",
+    "build:ssr": "cross-env BUILD_TARGET=node vue-cli-service build --no-clean",
+    "build:all": "npm run build:client && cross-env NODE_PORT=9080 npm run build:ssr",
+    "build": "cross-env NODE_DEPLOY=prod npm run build:all",
+
+    "lint": "vue-cli-service lint",
+    "dist": "cross-env NODE_DEPLOY=t1 NODE_ENV=production node ./dist/server"
+  }
+```
+
 ### 改造 vue.config.js
+
+引入各种环境，配置。
+
+导出的对象增加设置：
+
+- publicPath
+  部署应用包时的基本 URL。默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上，例如 `https://www.my-app.com/`。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 `https://www.my-app.com/my-app/`，则设置 publicPath 为 /my-app/。
+  这个值也可以被设置为空字符串 ('') 或是相对路径 ('./')，这样所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径，也可以用在类似 Cordova hybrid 应用的文件系统中。
+
+- assetsDir：放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+- configureWebpack.plugin[]：增加 webpack.DefinePlugin(),生产环境时加上 CopyWebpackPlugin()复制必要的文件去 dist 目录
+- chainWebpack： 1.增加 alias 别名。2.config.plugin('html')替换 public/index.html 的模板。3.node 端修复没有 document 的问题。4.设置缓存名字，区别客户端
+
+待解决：
 
 - 无 Proxy 处理
 - 通过修改 baseUrl，修改 服务端渲染页面的前端静态资源地址，不够优雅，并且可能导致前端渲染刷新页面导致 404 无法访问
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,32 @@
 // vue.config.js
-
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 const nodeExternals = require('webpack-node-externals')
-const merge = require('lodash.merge')
-const TARGET_NODE = process.env.WEBPACK_TARGET === 'node'
+
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const deployConfig = require('./config')
+
+const path = require('path')
+const resolve = (file) => path.resolve(__dirname, file)
+
+const TARGET_NODE = process.env.BUILD_TARGET === 'node'
 const target = TARGET_NODE ? 'server' : 'client'
+const isDev = process.env.NODE_ENV && process.env.NODE_ENV.indexOf('dev') > -1
 
 module.exports = {
+  publicPath: deployConfig[`${isDev ? 'dev' : 'build'}`].assetsPublicPath,
+  assetsDir: 'static',
   css: {
     extract: process.env.NODE_ENV === 'production',
+    sourceMap: !isDev && !TARGET_NODE, // if enable sourceMap:  fix ssr load Critical CSS throw replace of undefind
   },
+  // devServer: {
+  //   headers: { 'Access-Control-Allow-Origin': '*' },
+  //   proxy: deployConfig.dev.proxyTable,
+  //   disableHostCheck: true //  新增该配置项 fix ssr console error
+  // },
+  transpileDependencies: [],
   configureWebpack: () => ({
     // 将 entry 指向应用程序的 server / client 文件
     entry: `./src/entry-${target}.js`,
@@ -30,7 +46,7 @@ module.exports = {
           // 不要外置化 webpack 需要处理的依赖模块。
           // 你可以在这里添加更多的文件类型。例如，未处理 *.vue 原始文件，
           // 你还应该将修改 `global`（例如 polyfill）的依赖模块列入白名单
-          allowlist: [/\.css$/],
+          allowlist: [/\.css$/, /\?vue&type=style/],
         })
       : undefined,
     optimization: {
@@ -38,16 +54,108 @@ module.exports = {
     },
     plugins: [
       TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin(),
-    ],
+      new webpack.DefinePlugin({
+        'process.env.VUE_ENV': `"${target}"`,
+        'process.env.NODE_DEPLOY': `"${process.env.NODE_DEPLOY}"`,
+        'process.env.config': getDeployConfigDefine(),
+      }),
+    ].concat(
+      isDev
+        ? []
+        : new CopyWebpackPlugin([
+            {
+              from: resolve('./static'),
+              to: resolve('./dist/static'),
+              toType: 'dir',
+              ignore: ['index.html', '.DS_Store'],
+            },
+            {
+              from: resolve('./server'),
+              to: resolve('./dist/server'),
+              toType: 'dir',
+              ignore: [
+                'setup-dev-server.js',
+                'pm2.config.template.js',
+                '.DS_Store',
+              ],
+            },
+            {
+              from: resolve('./server/pm2.config.template.js'),
+              to: resolve('./dist/server/pm2.config.js'),
+              transform: function (content) {
+                return content
+                  .toString()
+                  .replace('NODE_ENV_VALUE', process.env.NODE_ENV)
+                  .replace('NODE_PORT_VALUE', process.env.NODE_PORT)
+                  .replace('NODE_DEPLOY_VALUE', process.env.NODE_DEPLOY)
+              },
+            },
+            {
+              from: resolve('./package.json'),
+              to: resolve('./dist'),
+            },
+            {
+              from: resolve('./package-lock.json'),
+              to: resolve('./dist'),
+            },
+          ])
+    ),
   }),
   chainWebpack: (config) => {
+    // alias
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@assets', resolve('src/assets'))
+
+    // reset public/index.html to static/index.html
+    config.plugin('html').tap((args) => {
+      args[0].template = resolve('./static/index.html')
+      return args
+    })
+
+    // process.env.BUILD_TARGET === 'node'
+    if (TARGET_NODE) {
+      // fix ssr bug: document not found -- https://github.com/Akryum/vue-cli-plugin-ssr/blob/master/lib/webpack.js
+      const isExtracting = config.plugins.has('extract-css')
+      if (isExtracting) {
+        // Remove extract
+        const langs = ['css', 'postcss', 'scss', 'sass', 'less', 'stylus']
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+        for (const lang of langs) {
+          for (const type of types) {
+            const rule = config.module.rule(lang).oneOf(type)
+            rule.uses.delete('extract-css-loader')
+            // Critical CSS
+            rule
+              .use('vue-style')
+              .loader('vue-style-loader')
+              .before('css-loader')
+          }
+        }
+        config.plugins.delete('extract-css')
+      }
+
+      config.module
+        .rule('vue')
+        .use('cache-loader')
+        .tap((options) => {
+          // Change cache directory for server-side
+          options.cacheIdentifier += '-server'
+          options.cacheDirectory += '-server'
+          return options
+        })
+    }
+
     config.module
       .rule('vue')
       .use('vue-loader')
       .tap((options) => {
-        merge(options, {
-          optimizeSSR: false,
-        })
+        if (TARGET_NODE) {
+          options.cacheIdentifier += '-server'
+          options.cacheDirectory += '-server'
+        }
+        options.optimizeSSR = TARGET_NODE
+        return options
       })
 
     // fix ssr hot update bug
@@ -55,4 +163,13 @@ module.exports = {
       config.plugins.delete('hmr')
     }
   },
+}
+
+// deploy config converter
+function getDeployConfigDefine() {
+  let config = {}
+  Object.keys(deployConfig.env).forEach(function (key) {
+    config[key] = `"${deployConfig.env[key]}"`
+  })
+  return config
 }
